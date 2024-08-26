a72ddfd12d5fca459dc4b4c89fc169370642ef1f
Michelle Tan <dev.michelletan@gmail.com>
2024-08-26 08:10:46 +0700

    Update googleDriveConfig.ts

--- backend/src/config/googleDriveConfig.ts
+++ backend/src/config/googleDriveConfig.ts
@@ -3,8 +3,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+
+// Retrieve and decode the Base64 encoded key from environment variable
+const base64Key = process.env.GOOGLE_API_KEY_BASE64 || '';
+const keyFileContent = Buffer.from(base64Key, 'base64').toString('utf8');
+
+// Parse the JSON content
+const credentials = JSON.parse(keyFileContent);
+
 const auth = new google.auth.GoogleAuth({
-  keyFile: './gdapi_key.json',
+  credentials: {
+    client_email: credentials.client_email,
+    private_key: credentials.private_key
+  },
   scopes: ['https://www.googleapis.com/auth/drive'],
 });
 
